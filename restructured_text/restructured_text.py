4e3a2344de20266f17f53656a9bbc886102d3b70
Allen, Timothy <tallen@wharton.upenn.edu>
2015-10-13 13:30:40 +0000

    Add RST block.

---
+++ restructured_text/restructured_text.py
@@ -0,0 +1,15 @@
+from django.utils.safestring import mark_safe
+from docutils.core import publish_parts
+from wagtail.wagtailcore import blocks
+
+
+class RSTBlock(blocks.TextBlock):
+    """
+    A ReSTructured text block for Wagtail streamfields.
+    """
+
+    class Meta:
+        icon = 'code'
+
+    def render_basic(self, value):
+        return publish_parts(value, writer_name='html')['body']
